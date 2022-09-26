20a7e16e6f73c7ff27874f59bb84dd4ecc4b3693
Rafii2198 <rafii2198@gmail.com>
2022-09-26 22:51:00 +0200

    Ininital upload

---
+++ ArtemisUpdate.ps1
@@ -0,0 +1,28 @@
+$modPath = $args[0]
+$modPath = "$modPath/mods"
+$modloader = "args[1]"
+$jenkins = "https://ci.wynntils.com/job/Artemis/lastSuccessfulBuild/artifact/$modloader/build/libs/"
+$modname = "wynntils*-$modloader.jar"
+$exists = Test-Path -Path "$modPath/$modname" -PathType Leaf
+$connects = $true
+
+try {
+  $request = Invoke-WebRequest $jenkins
+}
+catch {
+  Write-Host "Could not connect to Jenkins. Can not continue."
+  $connects = $false
+}
+if ($connects) {
+  $filename = ($request.ParsedHtml.getElementsByTagName("a") | Where-Object { $_.innerText -Like $modname } | Select-Object -ExpandProperty nameProp).Trim("about:")
+  if ($exists) {
+    $mod = Get-ChildItem -Path $modPath -Name -Filter $modname
+    if (!($mod -eq $filename)) {
+      Remove-Item -Path "$modPath/$mod"
+      Invoke-WebRequest "$jenkins/$filename" -OutFile "$modPath/$filename"
+    }
+  }
+  elseif ($exists -eq $false) {
+    Invoke-WebRequest "$jenkins/$filename" -OutFile "$modPath/$filename"
+  }
+}
